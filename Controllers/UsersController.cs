c9ea84d6e9c0f18393c3ab580737fb2e3fa4617f
gumani-38 <surprisegumani@gmail.com>
2026-03-14 16:55:07 +0200

    added the google font, fixed the logo display

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -49,28 +49,28 @@ namespace Gumani_Moila_ST10229429_CLDV7111w.Controllers
             return View(user);
         }
 
-        // GET: Users/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+        //// GET: Users/Create
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
 
-        // POST: Users/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("UserId,UserName,UserLastName,UserEmail,UserPassword,UserPhone")] User user)
-        {
-            if (ModelState.IsValid)
-            {
-                user.UserPassword = BCrypt.Net.BCrypt.HashPassword(user.UserPassword);
-                _context.Add(user);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
-        }
+        //// POST: Users/Create
+        //// To protect from overposting attacks, enable the specific properties you want to bind to.
+        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("UserId,UserName,UserLastName,UserEmail,UserPassword,UserPhone")] User user)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        user.UserPassword = BCrypt.Net.BCrypt.HashPassword(user.UserPassword);
+        //        _context.Add(user);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(user);
+        //}
 
         // Post: users/login
         // allow user email and password
@@ -133,97 +133,97 @@ namespace Gumani_Moila_ST10229429_CLDV7111w.Controllers
         }
 
 
-        // GET: Users/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //// GET: Users/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var user = await _context.User.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return View(user);
-        }
+        //    var user = await _context.User.FindAsync(id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(user);
+        //}
 
         // POST: Users/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("UserId,UserName,UserLastName,UserPassword,UserPhone")] User posted)
-        {
-            if (id != posted.UserId)
-            {
-                return NotFound();
-            }
-            var user = await _context.User.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            if (ModelState.IsValid)
-            {
-                user.UserName = posted.UserName;
-                user.UserLastName = posted.UserLastName;
-                user.UserPhone = posted.UserPhone;
-                user.UserPassword = BCrypt.Net.BCrypt.HashPassword(posted.UserPassword);
-                try
-                {
+        //public async Task<IActionResult> Edit(int id, [Bind("UserId,UserName,UserLastName,UserPassword,UserPhone")] User posted)
+        //{
+        //    if (id != posted.UserId)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var user = await _context.User.FindAsync(id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    if (ModelState.IsValid)
+        //    {
+        //        user.UserName = posted.UserName;
+        //        user.UserLastName = posted.UserLastName;
+        //        user.UserPhone = posted.UserPhone;
+        //        user.UserPassword = BCrypt.Net.BCrypt.HashPassword(posted.UserPassword);
+        //        try
+        //        {
                     
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UserExists(user.UserId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
-        }
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!UserExists(user.UserId))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(user);
+        //}
 
         // GET: Users/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var user = await _context.User
-                .FirstOrDefaultAsync(m => m.UserId == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
+        //    var user = await _context.User
+        //        .FirstOrDefaultAsync(m => m.UserId == id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(user);
-        }
+        //    return View(user);
+        //}
 
         // POST: Users/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var user = await _context.User.FindAsync(id);
-            if (user != null)
-            {
-                _context.User.Remove(user);
-            }
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var user = await _context.User.FindAsync(id);
+        //    if (user != null)
+        //    {
+        //        _context.User.Remove(user);
+        //    }
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
         private bool UserExists(int id)
         {
